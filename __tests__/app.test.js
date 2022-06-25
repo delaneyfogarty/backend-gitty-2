14a800647594d83290384b02e01279a87bd4022c
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-24 17:54:35 -0700

    just getting started

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,13 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('should redirect to the github oauth page upon login', async () => {
+    const res = await request(app).get('/api/v1/github/login');
+
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
   });
   afterAll(() => {
     pool.end();
