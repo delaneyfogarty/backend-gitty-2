0b46bf06434445acb043796dbfccd169e8a516ed
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-11 16:46:51 -0700

    successfully redirecting user to the github oauth page upon login

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -12,9 +14,25 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/api/v1/github/login');
 
     expect(res.header.location).toMatch(
-      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+      `https://github.com/login/oauth/authorize?client_id=${process.env.GH_CLIENT_ID}&scope=user&redirect_`
     );
   });
+
+  it('login and redirect users to /api/v1/github/dashboard', async () => {
+    const resp = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
